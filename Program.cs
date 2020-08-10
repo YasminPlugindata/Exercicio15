c9698807aa7e1393ffaa5ec8559fdd9a8408c23e
Yasmin <yasmin@plugindata.com.br>
2020-08-10 11:51:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Exercicio15 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++) { 
+                vet[1] = int.Parse(s[1]);
+            }
+
+            for (int i = 0; i < N; i++) { 
+                if (vet[i] < 0) { 
+                    Console.WriteLine(vet[1]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
